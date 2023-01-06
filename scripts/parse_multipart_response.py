785dca77eda2b256d2ea39cf366e42b289002b71
Catherine Weng <catherine@milvue.com>
2023-01-06 10:55:54 +0100

    fix boundary value and file type double conversion

--- scripts/parse_multipart_response.py
+++ scripts/parse_multipart_response.py
@@ -5,18 +5,17 @@ python parse_multipart_response.py response_content.txt
 import sys
 from io import BytesIO
 
-import pydicom
 from milvue_sdk.utils import decode_multipart
 
 
 with open(sys.argv[1], "rb") as f:
     file_bytes = f.read()
 
+boundary = file_bytes[2:34].decode()
+content_type = f"multipart/related; boundary={boundary}"
 
-dcm_list = decode_multipart(file_bytes, "multipart/related")
-
-for dcm_bytes in dcm_list:
-    dcm = pydicom.dcmread(BytesIO(dcm_bytes))
+dcm_list = decode_multipart(file_bytes, content_type)
+for dcm in dcm_list:
     name = f"{dcm.Modality}.{dcm.SOPInstanceUID}.dcm"
     dcm.save_as(name)
     print(name)
