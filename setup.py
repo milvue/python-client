5b845dfbd59fdb68b773fad38b4e6e9a5238100d
Setra Rakotovao <rakotovaosetra@outlook.com>
2023-10-18 16:03:55 +0200

    Remove python-dotenv dependency

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     zip_safe=False,
-    install_requires=["pydantic", "pydicom", "python-dotenv", "requests_toolbelt"],
+    install_requires=["pydantic", "pydicom", "requests_toolbelt"],
 )
