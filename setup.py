12653589f8c18e4f68acec74cf3af1cf10114006
Setra Rakotovao <77625515+setra022@users.noreply.github.com>
2023-12-05 16:18:29 +0100

    Minor fixes (#4)
    * Minor fixes
    
    * Remove python-dotenv dependency
    
    * Renaming
    
    * Update readme
    
    * Update readme

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     zip_safe=False,
-    install_requires=["pydantic", "pydicom"],
+    install_requires=["pydantic", "pydicom", "requests_toolbelt"],
 )
